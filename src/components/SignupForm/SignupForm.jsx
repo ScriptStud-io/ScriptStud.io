719047bc68514cd58a5c77b06c10bd62dcebcc5e
cmosley <cmosley@beholdmedia.com>
2021-04-18 18:33:43 -0500

    sign-up page

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -46,50 +46,101 @@ class SignupForm extends Component {
   render() {
     const { name, email, password, passwordConf } = this.state;
     return (
-      <div>
-        <h3>Sign Up</h3>
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            autoComplete="off"
-            id="name"
-            value={name}
-            name="name"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            autoComplete="off"
-            id="email"
-            value={email}
-            name="email"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="password"
-            value={password}
-            name="password"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="confirm"
-            value={passwordConf}
-            name="passwordConf"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="confirm">Confirm Password</label>
-          <button disabled={this.isFormInvalid()}>Sign Up</button>
-          &nbsp;&nbsp;
-          <Link to="/">Cancel</Link>
-        </form>
+    <main className="LoginPage">
+      <div className="mt-10 sm:mt-0">
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+          {/* <div className="md:col-span-1">
+            
+          </div> */}
+          <div className="mt-5 md:mt-10 md:col-span-2">
+            <form autoComplete="off" onSubmit={this.handleSubmit}>
+              <div className="shadow overflow-hidden sm:rounded-md">
+                <div className="px-4 py-5 bg-white sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-6">
+                      <label htmlFor="first_name" className="block text-sm font-medium text-gray-700">
+                        Name
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        autoComplete="off"
+                        value={name}
+                        onChange={this.handleChange}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                   
+                    
+                    <div className="col-span-6 sm:col-span-6">
+                      <label htmlFor="email_address" className="block text-sm font-medium text-gray-700">
+                        Email address
+                      </label>
+                      <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        autoComplete="off"
+                        value={email}
+                        onChange={this.handleChange}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                    
+       
+
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-3">
+                      <label htmlFor="state" className="block text-sm font-medium text-gray-700">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={password}
+                        onChange={this.handleChange}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                    
+                    <div className="col-span-6 sm:col-span-3 lg:col-span-3">
+                      <label htmlFor="postal_code" className="block text-sm font-medium text-gray-700">
+                        Confirm Password
+                      </label>
+                      <input
+                        type="password"
+                        autoComplete="off"
+                        id="confirm"
+                        value={passwordConf}
+                        name="passwordConf"
+                        onChange={this.handleChange}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                    
+            
+          
+                  </div>
+                </div>
+                    <div className="px-4 py-3 bg-gray-50 sm:px-6">
+                      <button
+                        disabled={this.isFormInvalid()}
+                        type="submit"
+                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        Sign Up
+                      </button>
+                      <Link to="/">
+                      <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-6">Cancel</button>
+                      </Link>
+                   </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
+    </main>
     );
   }
 }
