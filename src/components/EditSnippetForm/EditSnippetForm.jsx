042dab4f6f4225df76da9dd0af76a89b57d1973f
mhsmith321 <marty.smith01@yahoo.com>
2021-04-19 12:02:22 -0400

    merge snippet page into org form

--- src/components/EditSnippetForm/EditSnippetForm.jsx
+++ src/components/EditSnippetForm/EditSnippetForm.jsx
@@ -154,8 +154,6 @@ export default function EditSnippetForm(props){
                         type="submit"
                         disabled={formInvalid}
                         className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        type='submit'
-                        disabled={state.formInvalid}
                         >EDIT</button> 
                         <Link to="/snippets">
                         <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-6">Cancel</button>
--- src/components/SnippetPreview/SnippetPreview.jsx
+++ src/components/SnippetPreview/SnippetPreview.jsx
@@ -1,7 +1,20 @@
 import './SnippetPreview.css';
 
-function SnippetPreview(props) {
-    return <h2>This is the snippet preview component.</h2>
+function SnippetPreview() {
+    return (
+        <div className='snip-preview'>
+            <div className='snip-preview-logo'></div>
+            <div className='snip-preview-content'>
+
+                <div className='snip-preview-title'>Using .push() in JavaScript</div>
+                <div>Spicy jalapeno bacon ipsum dolor amet shank ham andouille filet mignon, landjaeger chislic tri-tip picanha brisket. Kielbasa capicola flank, chicken bresaola short ribs meatball alcatra pork belly. Leberkas cupim pancetta tenderloin...</div>
+                <div>
+                    Created by: Marty Smith | Created on: 14 April 2021 | Last Edited: 16 April 2021
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default SnippetPreview;
