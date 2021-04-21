633fbcdeffcaf129cc9d15e709497912554f60ee
mhsmith321 <marty.smith01@yahoo.com>
2021-04-21 13:34:21 -0400

    uncommented necessary code

--- src/components/CodeEditor/CodeEditor.jsx
+++ src/components/CodeEditor/CodeEditor.jsx
@@ -1,33 +1,33 @@
-// import React from "react";
-// import AceEditor from "react-ace";
-// import "ace-builds/src-noconflict/theme-github";
-// import "ace-builds/src-noconflict/theme-monokai";
+import React from "react";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-monokai";
 
-// export default function CodeEditor(props) {
+export default function CodeEditor(props) {
 
-//         return (
-//         <div id="editor">
-//                 <AceEditor
-//                 placeholder="Insert Snippet"
-//                 mode="javascript"
-//                 theme="monokai"
-//                 name={props.name}
-//                 height="250px"
-//                 width="515px"
-//                 // onLoad={this.onLoad}
-//                 onChange={(e) => props.handleChange(e, props.name)}
-//                 fontSize={16}
-//                 showPrintMargin={true}
-//                 showGutter={true}
-//                 highlightActiveLine={true}
-//                 value={props.state[props.name]}
-//                 setOptions={{
-//                 enableBasicAutocompletion: false,
-//                 enableLiveAutocompletion: true,
-//                 enableSnippets: true,
-//                 showLineNumbers: true,
-//                 tabSize: 2,
-//                 }}/>
-//         </div>
-//         )
-// }
+        return (
+        <div id="editor">
+                <AceEditor
+                placeholder="Insert Snippet"
+                mode="javascript"
+                theme="monokai"
+                name={props.name}
+                height="250px"
+                width="515px"
+                // onLoad={this.onLoad}
+                onChange={(e) => props.handleChange(e, props.name)}
+                fontSize={16}
+                showPrintMargin={true}
+                showGutter={true}
+                highlightActiveLine={true}
+                value={props.state[props.name]}
+                setOptions={{
+                enableBasicAutocompletion: false,
+                enableLiveAutocompletion: true,
+                enableSnippets: true,
+                showLineNumbers: true,
+                tabSize: 2,
+                }}/>
+        </div>
+        )
+}
