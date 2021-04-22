80e0ae7f92acec5a412e9c1e4cdd2d74052d436c
cmosley <cmosley@beholdmedia.com>
2021-04-21 21:37:23 -0500

    snippet styling

--- src/components/SyntaxHighlighter/SyntaxHighlighter.jsx
+++ src/components/SyntaxHighlighter/SyntaxHighlighter.jsx
@@ -1,9 +1,10 @@
+import './SyntaxHighlighter.css'
 import { Prism } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function SyntaxHighlighter(props) {
     console.log('props:', props)
     return(
-    <Prism language='javascript' style={dark}>{props.state[props.name]}</Prism>
+    <Prism id='test' language='javascript' style={dark}>{props.state[props.name]}</Prism>
     )
 }
