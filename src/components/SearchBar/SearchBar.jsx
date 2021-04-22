8472c0f32a1bb27157673d092afed523f8e55cb4
cmosley <cmosley@beholdmedia.com>
2021-04-22 10:13:49 -0500

    search implemented

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,27 +1,21 @@
 import { Checkbox } from 'semantic-ui-react';
 import './SearchBar.css';
-import React, { useState, useEffect } from 'react';
-import { useForm } from '../../hooks/useForm'
-
 
 
 const SearchBar = (props) => {
-    const [search, setSearch] = useForm()
-
-    console.log(search)
-
+     
     return (
         <div className="flex">
+        {/* <form ref={formRef} onSubmit={handleSubmit}> */}
         <form>
           <label htmlFor="search" className="sr-only">
             Search
           </label>
           <input
             type="text"
-            name="search"
+            name="text"
             id="search"
-            value={search}
-            onChange={setSearch}
+            onChange={props.setSearch}
             className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md h-7 px-2"
           />
           {/* <Checkbox>My Snips</Checkbox> */}
--- src/pages/SearchResultsPage/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -9,26 +9,36 @@ import { useForm } from '../../hooks/useForm'
 const SearchResultsPage = props => {
 
     const [allSnippets, setAllSnippets] = useState([]);
+    const [search, setSearch] = useForm('')
+    const [filteredResults, setFilteredResults] = useState([]);
     
 
     useEffect(() => {
         (async function() {
             const snippets = await snippetAPI.getAll();
             setAllSnippets(snippets);
+            console.log('snippets:', snippets)
         })();
     }, [])
 
-    console.log('allSnippets: ', allSnippets)
+    useEffect(() => {
+        setFilteredResults(
+            allSnippets.filter(snippet => (
+              snippet.title.toLowerCase().includes(search.text.toLowerCase())
+            ))   
+        )      
+    }, [search])   
 
     return (
         <>
         <section className="w-3/4 mt-6 mx-auto">
             <div id="search" className="w-2/5 flex items-center mb-4">
                <p>Search: &nbsp;</p> 
-               <SearchBar />
+               <SearchBar search={search} setSearch={setSearch}/>
             </div>
             <ul className="space-y-3">
-            {allSnippets.map((snip, idx) => (
+            {/* {filteredResults.map((snip, idx) => ( */}
+            {filteredResults.map((snip, idx) => (
                 <li key={idx} className="bg-white shadow overflow-auto rounded-md px-6 py-4">
                     <SnippetPreview key={idx} data={snip} />
                 </li>
