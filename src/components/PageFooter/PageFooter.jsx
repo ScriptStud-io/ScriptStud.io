58e2b82adf6325068fa28ae808c068c52b4e96ec
cmosley <cmosley@beholdmedia.com>
2021-04-23 00:36:09 -0500

    fix text wrapping on syntax highligher, footer spacing on search results

--- src/components/PageFooter/PageFooter.jsx
+++ src/components/PageFooter/PageFooter.jsx
@@ -51,7 +51,7 @@ const navigation = {
     return (
       <footer id="footer" className="bg-white ">
         <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-          <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+          {/* <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             {navigation.main.map((item) => (
               <div key={item.name} className="px-5 py-2">
                 <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
@@ -59,7 +59,7 @@ const navigation = {
                 </a>
               </div>
             ))}
-          </nav>
+          </nav> */}
           <div className="mt-8 flex justify-center space-x-6">
             {navigation.social.map((item) => (
               <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
@@ -67,7 +67,7 @@ const navigation = {
                 <item.icon className="h-6 w-6" aria-hidden="true" />
               </a>
             ))}
-          </div>
+          </div> 
           <p className="mt-8 text-center text-base text-gray-400">&copy; 2021 ScriptStud.io, Inc. All rights reserved.</p>
         </div>
       </footer>
