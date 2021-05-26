6a4197e363560c7b728e7599e11b62c1c3dea46f
mhsmith321 <marty.smith01@yahoo.com>
2021-05-26 14:57:56 -0400

    able to toggle isPrivate checkbox on edit form

--- src/components/EditSnippetForm/EditSnippetForm.jsx
+++ src/components/EditSnippetForm/EditSnippetForm.jsx
@@ -154,7 +154,7 @@ export default function EditSnippetForm(props){
                             type="checkbox"
                             name="isPrivate"
                             id="isPrivate"
-                            checked={props.currentSnippet.isPrivate}
+                            defaultChecked={props.currentSnippet.isPrivate}
                             onChange={setState}
                             pattern=".{2,}"
                             className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md px-2"
@@ -175,4 +175,3 @@ export default function EditSnippetForm(props){
         </main>
         );
     }
- 
